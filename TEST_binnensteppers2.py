05c1a0cdb53febad0b5fe081d8b04e3c71933433
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-06-02 20:40:10 +0200

    Update TEST_binnensteppers2.py

--- TEST_binnensteppers2.py
+++ TEST_binnensteppers2.py
@@ -68,7 +68,6 @@ aantal_graden_nulpunt_aarde = 124 # begin magneet veld = 20 augustus
 aantal_graden_te_lopen = aantal_graden_positiestring - aantal_graden_nulpunt_aarde 
 aantal_stappen_te_lopen = int(aantal_graden_te_lopen * 57.34)
 
-
 try:
   while (schakelaar == "open"):
     for pin in list(range(0, 4)):
@@ -102,5 +101,3 @@ try:
 except KeyboardInterrupt:
   # GPIO netjes afsluiten
   # GPIO.cleanup()
-
-

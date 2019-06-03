11e4c5ff9b2476a393364d22ef74221633f4cebf
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-06-03 20:41:02 +0200

    Update TEST_binnensteppers2.py

--- TEST_binnensteppers2.py
+++ TEST_binnensteppers2.py
@@ -61,7 +61,7 @@ import requests
 url = 'http://planetarium.chrisdemoor.nl/positions.txt'
 r = requests.get(url)
 positiestring = r.text
-aantal_graden_positiestring = int(positiestring[6:9])
+aantal_graden_positiestring = 360 - int(positiestring[6:9])
 aantal_graden_nulpunt_aarde = 124 # begin magneet veld = 20 augustus
 aantal_graden_te_lopen = aantal_graden_positiestring - aantal_graden_nulpunt_aarde 
 aantal_stappen_te_lopen = int(aantal_graden_te_lopen * 57.34)

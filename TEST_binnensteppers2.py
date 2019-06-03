1e54db9326ab3f1008b3f9224b5b29b27a81a789
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-06-03 20:00:48 +0200

    Update TEST_binnensteppers2.py

--- TEST_binnensteppers2.py
+++ TEST_binnensteppers2.py
@@ -66,7 +66,6 @@ aantal_graden_nulpunt_aarde = 124 # begin magneet veld = 20 augustus
 aantal_graden_te_lopen = aantal_graden_positiestring - aantal_graden_nulpunt_aarde 
 aantal_stappen_te_lopen = int(aantal_graden_te_lopen * 57.34)
 
-
 try:
   # EERST RONDJE AFMAKEN EN NAAR HET BEGIN LOPEN
   teller = 1

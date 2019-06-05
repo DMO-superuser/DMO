83ecde5e8a8df38f317830c01ab3887b01b907f7
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-06-05 09:45:22 +0200

    Update TEST_buitensteppers_rondje_stop.py

--- TEST_buitensteppers_rondje_stop.py
+++ TEST_buitensteppers_rondje_stop.py
@@ -1,4 +1,14 @@
+# spullen reedswitch
+import os
+import wiringpi
+from time import sleep
+os.system('gpio export 26 in')
+sleep(0.5)
+io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_GPIO_SYS)
+io.pinMode(26,io.INPUT)
 
+schakelaar = "open"
+teller = 1
     
 """Simple test for using adafruit_motorkit with a stepper motor"""
 from adafruit_motorkit import MotorKit
@@ -7,5 +17,18 @@ from time import sleep
 
 kit = MotorKit()
 
-while True:
+while (schakelaar == "open"):
   kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+  if (io.digitalRead(26)):
+    #print ("open")
+    schakelaar = "open"
+  else:
+    # onder de schakelaar
+    #print ("dicht")
+    #print (teller)
+    if teller > 1500:
+      schakelaar = "dicht"
+
+    teller +=1
+    
+print (teller)

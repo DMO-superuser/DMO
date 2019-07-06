60c386cbe074faedf4a4648566fc7254e96af641
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-07-06 13:41:24 +0200

    Update DMO_buiten.py
    release()

--- DMO_buiten.py
+++ DMO_buiten.py
@@ -118,7 +118,7 @@ while True:
     sleep (5)
    
     # motoren loslaten
-    stepper1.release(stepper1)
+    kit.stepper.release()
    
     positiestring_oud = positiestring
     schakelaar = "open"

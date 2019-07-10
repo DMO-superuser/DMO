a1aef526cafacb1329594da5294df2a9df2ae5ad
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-07-10 19:29:47 +0200

    Update DMO_buiten.py

--- DMO_buiten.py
+++ DMO_buiten.py
@@ -21,37 +21,7 @@ if (planeet == "DMO-Mars"):
    stappen_per_graad = totaal_stappen / 360   
    beginpos_string = 9  # de beginpositie in de string bij de Curl van deze planeet
    eindpos_string  = 12  # de eindpositie in de string bij de Curl van deze planeet
-if (planeet == "DMO-Aarde"):
-   steppersoort = "binnen"    # er bestaan binnen- en buitensteppers 
-   totaal_stappen = 2000 # aantal stappen om een rondje te maken, 1% afwijking per keer
-   begin_stappen = 14   # het magneetje ligt op een willekeurige plek in het planetarium, dat is niet noodzakelijkerwijs het begin van de graden-berekening
-   # Mercurius 0 en 3, Venus 3 en 6, Aarde 6 en 9, Mars 9 en 12, Jupiter 12 en 15, Saturnus 15 en 18
-   beginpos_string = 6  # de beginpositie in de string bij de Curl van deze planeet
-   eindpos_string  = 9  # de eindpositie in de string bij de Curl van deze planeet
-if (planeet == "DMO-Venus"):
-   steppersoort = "binnen"    # er bestaan binnen- en buitensteppers 
-   totaal_stappen = 20778 # aantal stappen om een rondje te maken, 1% afwijking per keer, bij Seq2 en Stepcount2
-   #totaal_stappen = 10591 # aantal stappen om een rondje te maken, 1% afwijking per keer, bij Seq1 en Stepcount1
-   begin_stappen = 14   # het magneetje ligt op een willekeurige plek in het planetarium, dat is niet noodzakelijkerwijs het begin van de graden-berekening
-   # Mercurius 0 en 3, Venus 3 en 6, Aarde 6 en 9, Mars 9 en 12, Jupiter 12 en 15, Saturnus 15 en 18
-   beginpos_string = 6  # de beginpositie in de string bij de Curl van deze planeet
-   eindpos_string  = 9  # de eindpositie in de string bij de Curl van deze planeet      
-if (planeet == "DMO-Mercurius"):
-   steppersoort = "binnen"    # er bestaan binnen- en buitensteppers 
-   totaal_stappen = 4099 # aantal stappen om een rondje te maken, 1% afwijking per keer, bij Seq2 en Stepcount2
-   #totaal_stappen = 2067 # aantal stappen om een rondje te maken, 1% afwijking per keer, bij Seq1 en Stepcount1
-   begin_stappen = 14   # het magneetje ligt op een willekeurige plek in het planetarium, dat is niet noodzakelijkerwijs het begin van de graden-berekening
-   # Mercurius 0 en 3, Venus 3 en 6, Aarde 6 en 9, Mars 9 en 12, Jupiter 12 en 15, Saturnus 15 en 18
-   beginpos_string = 6  # de beginpositie in de string bij de Curl van deze planeet
-   eindpos_string  = 9  # de eindpositie in de string bij de Curl van deze planeet
-if (planeet == "DMO-Aarde"):
-   steppersoort = "binnen"    # er bestaan binnen- en buitensteppers 
-   totaal_stappen = 20645 # aantal stappen om een rondje te maken, 1% afwijking per keer, bij Seq2 en Stepcount2
-   #totaal_stappen = 10335 # aantal stappen om een rondje te maken, 1% afwijking per keer, bij Seq1 en Stepcount1
-   begin_stappen = 14   # het magneetje ligt op een willekeurige plek in het planetarium, dat is niet noodzakelijkerwijs het begin van de graden-berekening
-   # Mercurius 0 en 3, Venus 3 en 6, Aarde 6 en 9, Mars 9 en 12, Jupiter 12 en 15, Saturnus 15 en 18
-   beginpos_string = 6  # de beginpositie in de string bij de Curl van deze planeet
-   eindpos_string  = 9  # de eindpositie in de string bij de Curl van deze planeet      
+ 
 
 # spullen reedswitch
 import os

f7df158c4833854091b49fed93b43cf0f3a37894
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-09-29 21:38:21 +0200

    Update WEER_test_steppers.py

--- WEER_test_steppers.py
+++ WEER_test_steppers.py
@@ -3,7 +3,7 @@ from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 from time import sleep
 
-kit = MotorKit("addr=1x61")
+kit = MotorKit(1x61)
 
 while True:
   kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)

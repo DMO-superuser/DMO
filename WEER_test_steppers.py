8f20dcf3f87e77c351d7823e83e2d19228172c21
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-09-29 16:37:09 +0200

    Update WEER_test_steppers.py

--- WEER_test_steppers.py
+++ WEER_test_steppers.py
@@ -1,13 +1,11 @@
 # WEER_test_steppers.py
 # 29 sep 2019, python3
 
-# spullen van de buitenstepper
+
 from adafruit_motorkit import MotorKit
-from adafruit_motor import stepper
-kit = MotorKit(address=0x61)
 
+kit = MotorKit()
 
-while True: 
-
-  kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+for i in range(100):
+    kit.stepper1.onestep()
 

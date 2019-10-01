3005db335a75600b8a3564d27f4b01599ea7145c
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-01 17:54:08 +0200

    Update WEER_test_steppers.py

--- WEER_test_steppers.py
+++ WEER_test_steppers.py
@@ -6,7 +6,7 @@ from time import sleep
 
 kit1 = MotorKit(address=0x60)
 # Initialise the second hat on a different address
-# kit2 = MotorKit(address=0x61)
+kit2 = MotorKit(address=0x61)
 
 while True:
   kit1.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)

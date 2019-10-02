538dba20814ddceb483ea1fbb0078a0ff6fe9e25
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-02 13:26:19 +0200

    Update WEER_systeem.py

--- WEER_systeem.py
+++ WEER_systeem.py
@@ -19,29 +19,39 @@ apache_indexfile = "/var/www/html/index.html"
 log_regel = open(apache_indexfile, "w")
 
 # binnentemperatuur meter
-import glob
+import sys
 
-while True:
+#while True:
 
   ########################
   # BINNENTEMPERATUUR B0M1
   ########################
-  # single coil steps, 500 stappen rond
-  devicedir = glob.glob("/sys/bus/w1/devices/28-00000809ac91/")
-  device = devicedir[0] + "w1_slave"
-  # open up the file - temperatuur met DS18B20
-  f = open (device, 'r')
-  sensor = f.readlines()
-  f.close()
-  # parse results from file - temperatuur met DS18B20
-  # temperatuur wordt op een 10e graad uitgelezen
-  # single coil steps, 513 stappen rond
-  # range = van -10 tot 40 graden, 500 stappen nodig. 100 stappen per 10 graden
-  # ijkpunt = 0 + 6 voor de eerste, hij begint op -10 graden C (= 100 stappen) 
-  crc = sensor[0].split()[-1]
-  temp = float(sensor[1].split()[-1].strip('t='))
-  temp_C = (temp/1000.000)
-  temp_C = round(temp_C,1)
-  log_regel.write("<p> Dit is de eerste regel </p>")
-  log_regel.write(temp_C)
+
+
+temp = {}
+ 
+sensorids = ["28-0000054871cb"]
+# loop net zo lang alles sensors af dat in het array hieboven staan.
+for sensor in range(len(sensorids)):
+  #tfile = open("/sys/bus/w1/devices/"+ sensorids[sensor] +"/w1_slave") #RPi 1,2 met oude kernel.
+  tfile = open("/sys/bus/w1/devices/"+ sensorids[sensor] +"/w1_slave") #RPi 2,3 met nieuwe kernel.
+  # Lees alle dat uit het "bestand" in een variabele.
+  text = tfile.read()
+  # Sluit het "bestand" nadat we het gelezen hebben.
+  tfile.close()
+  # We gaan nu de tekst splitsen per nieuwe regel (\n)
+  # en we selecteren de 2e regel [1] (1e regel = [0])
+  secondline = text.split("\n")[1]
+  # Splits de regel in "woorden", er wordt gespleten op de spaties.
+  # We selecteren hier het 10 "woord" [9] (tellend vanaf 0)
+  temperaturedata = secondline.split(" ")[9]
+  # De eerste 2 karakters zijn "t=", deze moeten we weghalen.
+  # we maken meteen van de string een integer (nummer).
+  temperature = float(temperaturedata[2:])
+  # De temperatuurwaarde moeten we delen door 1000 voor de juiste waarde.
+  temp[sensor] = temperature / 1000
+  # print de gegevens naar de console.
+  print "sensor", sensor, "=", temp[sensor], "graden."
+  #log_regel.write("<p> Dit is de eerste regel </p>")
+  #log_regel.write(temp_C)
 

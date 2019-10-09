13036240206ff623de54cd8bf33eb7beb0bb59a4
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-09 14:57:57 +0200

    Update WEER_test_steppers.py

--- WEER_test_steppers.py
+++ WEER_test_steppers.py
@@ -6,7 +6,7 @@ from time import sleep
 
 kit1 = MotorKit(address=0x60)
 kit2 = MotorKit(address=0x61)
-#kit3 = MotorKit(address=0x62)
+kit3 = MotorKit(address=0x62)
 #kit4 = MotorKit(address=0x63)
 
 while True:
@@ -14,8 +14,8 @@ while True:
   kit1.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
   kit2.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
   kit2.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-  #kit3.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-  #kit3.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+  kit3.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+  kit3.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
   #kit4.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
   #kit4.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
  

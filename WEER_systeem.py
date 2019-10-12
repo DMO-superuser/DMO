cedaa299079965515030b5a8f14764756dc673e9
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-12 20:18:41 +0200

    Update WEER_systeem.py

--- WEER_systeem.py
+++ WEER_systeem.py
@@ -9,13 +9,13 @@ GPIO.setup(4, GPIO.IN)  # pin7  binnen temperatuur
 
 GPIO.setup(22, GPIO.IN) # pin15 rode knop
 GPIO.setup(20, GPIO.IN) # pin38 reset 1
-GPIO.setup(16, GPIO.IN) # pin36 reset 2
-GPIO.setup(12, GPIO.IN) # pin32 reset 3
-GPIO.setup(7, GPIO.IN)  # pin26 reset 4
-GPIO.setup(8, GPIO.IN)  # pin24 reset 5
-GPIO.setup(25, GPIO.IN) # pin22 reset 6
+GPIO.setup(25, GPIO.IN) # pin36 reset 2
+GPIO.setup(16, GPIO.IN) # pin32 reset 3
+GPIO.setup(5, GPIO.IN)  # pin26 reset 4
+GPIO.setup(12, GPIO.IN)  # pin24 reset 5
+GPIO.setup(7, GPIO.IN) # pin22 reset 6
 GPIO.setup(11, GPIO.IN) # pin22 reset 7
-GPIO.setup(5, GPIO.IN)  # pin29 reset 8
+GPIO.setup(8, GPIO.IN)  # pin29 reset 8
 GPIO.setup(23, GPIO.IN) # pin16 windsnelheid
 
 GPIO.setwarnings(False)
@@ -69,9 +69,9 @@ while True:
         for x in range(0, 50): kit2.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE) 
   binnen_temp_oud = binnen_temp
   
-  # resetten van meter (zwarte knop)
+  # resetten van meter (zwarte knop bij desbetreffende meter)
   # zwarte knop ingedrukt houden, rode knop erbij om meter andere kant op te laten draaien
-  while (GPIO.input(12) == 0):
+  while (GPIO.input(16) == 0):
         # rode knop voor heen en weer
         if (GPIO.input(22) == 1):
            kit2.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE) 

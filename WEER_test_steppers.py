e19d2d3729eb2b9e37a7180acc43b722ba020c17
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-12 20:23:36 +0200

    Update WEER_test_steppers.py

--- WEER_test_steppers.py
+++ WEER_test_steppers.py
@@ -9,13 +9,13 @@ kit2 = MotorKit(address=0x61)
 kit3 = MotorKit(address=0x62)
 kit4 = MotorKit(address=0x63)
 
-while True:
-  kit1.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-  kit1.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-  kit2.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-  kit2.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-  kit3.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-  kit3.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-  kit4.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-  kit4.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+#while True:
+#  kit1.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+#  kit1.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+#  kit2.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+#  kit2.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+#  kit3.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+#  kit3.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+#  kit4.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+#  kit4.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
  

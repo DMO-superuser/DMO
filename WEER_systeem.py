32d526047e5cd00f2538ca0d5a4807ca245cee94
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-12 20:28:16 +0200

    Update WEER_systeem.py

--- WEER_systeem.py
+++ WEER_systeem.py
@@ -74,9 +74,9 @@ while True:
   while (GPIO.input(16) == 0):
         # rode knop voor heen en weer
         if (GPIO.input(22) == 1):
-           kit1.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE) 
+           kit1.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.MICROSTEP) 
         else:
-           kit1.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE) 
+           kit1.stepper1.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP) 
         binnen_temp_oud = -30
 
   ########################

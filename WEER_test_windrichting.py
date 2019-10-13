8242d608415013a46aff2da3f4d8d412a4bb9b62
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-13 11:34:07 +0200

    Create WEER_test_windrichting.py

---
+++ WEER_test_windrichting.py
@@ -0,0 +1,43 @@
+# diversen
+from time import sleep
+import os
+
+# GPIO
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN)  # pin7  binnen temperatuur 
+
+
+GPIO.setup(6, GPIO.IN) # windrichting 1
+GPIO.setup(13, GPIO.IN) # windrichting 2
+GPIO.setup(19, GPIO.IN) # windrichting 3
+GPIO.setup(26, GPIO.IN) # windrichting 4
+
+GPIO.setwarnings(False)
+
+
+
+while True:
+
+
+      
+  if (GPIO.input(6) == 0):
+     print ("6, windrichting 1 = 0")
+  else:
+     print ("6, windrichting 1 = 1")
+     
+  if (GPIO.input(13) == 0):
+     print ("13, windrichting 1 = 0")
+  else:
+     print ("13, windrichting 1 = 1")   
+          
+  if (GPIO.input(19) == 0):
+     print ("19, windrichting 1 = 0")
+  else:
+     print ("19, windrichting 1 = 1")     
+
+          
+  if (GPIO.input(26) == 0):
+     print ("26, windrichting 1 = 0")
+  else:
+     print ("26, windrichting 1 = 1")     

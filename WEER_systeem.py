ea36ce91fff7bac92bf66cb1223f6fd0f754b094
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-13 20:39:06 +0200

    Update WEER_systeem.py

--- WEER_systeem.py
+++ WEER_systeem.py
@@ -169,7 +169,7 @@ while True:
            reed_switch = 0
   km_per_uur = int(0.9 * aantal_nullen)
   
-  print("<p> De windsnelheid in km/h is " + str(aantal_nullen) + " </p>")
+  print("<p> De windsnelheid in km/h is " + str(km_per_uur) + " </p>")
 
   if (km_per_uur != km_per_uur_oud):
      verschil = km_per_uur - km_per_uur_oud 
@@ -200,6 +200,7 @@ while True:
   #
   # windrichting, Oost is rechts
   # ijkpunt is op het Zuiden, positie = 0
+  # wordt alleen uitgevoerd als windsnelheid groter of gelijk aan 12 km/h is 
   #
   #  26 19 13 6  R   
   #  1  0  0  0  N   256
@@ -211,33 +212,30 @@ while True:
   #  1  0  0  1  NW  192
   #  0  0  1  1  ZW  64       
 
-  print (GPIO.input(26))
-  print (GPIO.input(19))
-  print (GPIO.input(13))
-  print (GPIO.input(6))
-  
-  if ((GPIO.input(26) == 1) and (GPIO.input(19) == 0) and (GPIO.input(13) == 0) and (GPIO.input(6) == 0)): 
-    positie = 256
-    print("<p> De wind komt uit het Noorden </p>")    
-  if ((GPIO.input(26) == 0) and (GPIO.input(19) == 0) and (GPIO.input(13) == 0) and (GPIO.input(6) == 1)): 
-    positie = 128
-    print("<p> De wind komt uit het Westen </p>")    
-  if ((GPIO.input(26) == 0) and (GPIO.input(19) == 0) and (GPIO.input(13) == 1) and (GPIO.input(6) == 0)):
-    positie = 0
-    print("<p> De wind komt uit het Zuiden </p>")    
-  if ((GPIO.input(26) == 0) and (GPIO.input(19) == 2) and (GPIO.input(13) == 0) and (GPIO.input(6) == 0)): 
-    positie = 384      
-    print("<p> De wind komt uit het Oosten </p>")    
-  if ((GPIO.input(26) == 1) and (GPIO.input(19) == 1) and (GPIO.input(13) == 0) and (GPIO.input(6) == 0)):
-    positie = 320
-    print("<p> De wind komt uit het Noordoosten </p>")       
-  if ((GPIO.input(26) == 0) and (GPIO.input(19) == 1) and (GPIO.input(13) == 1) and (GPIO.input(6) == 0)): 
-    positie = 448
-    print("<p> De wind komt uit het Zuidoosten </p>")     
-  if ((GPIO.input(26) == 1) and (GPIO.input(19) == 0) and (GPIO.input(13) == 0) and (GPIO.input(6) == 1)): 
-    positie = 192
-    print("<p> De wind komt uit het Noordwesten </p>")     
-  if ((GPIO.input(26) == 0) and (GPIO.input(19) == 0) and (GPIO.input(13) == 1) and (GPIO.input(6) == 1)): 
-    positie = 64
-    print("<p> De wind komt uit het Zuidwesten </p>")         
-    
+  if (km_per_uur >= 12):
+    if ((GPIO.input(26) == 1) and (GPIO.input(19) == 0) and (GPIO.input(13) == 0) and (GPIO.input(6) == 0)): 
+      positie = 256
+      print("<p> De wind komt uit het Noorden </p>")    
+    if ((GPIO.input(26) == 0) and (GPIO.input(19) == 0) and (GPIO.input(13) == 0) and (GPIO.input(6) == 1)): 
+      positie = 128
+      print("<p> De wind komt uit het Westen </p>")    
+    if ((GPIO.input(26) == 0) and (GPIO.input(19) == 0) and (GPIO.input(13) == 1) and (GPIO.input(6) == 0)):
+      positie = 0
+      print("<p> De wind komt uit het Zuiden </p>")    
+    if ((GPIO.input(26) == 0) and (GPIO.input(19) == 2) and (GPIO.input(13) == 0) and (GPIO.input(6) == 0)): 
+      positie = 384      
+      print("<p> De wind komt uit het Oosten </p>")    
+    if ((GPIO.input(26) == 1) and (GPIO.input(19) == 1) and (GPIO.input(13) == 0) and (GPIO.input(6) == 0)):
+      positie = 320
+      print("<p> De wind komt uit het Noordoosten </p>")       
+    if ((GPIO.input(26) == 0) and (GPIO.input(19) == 1) and (GPIO.input(13) == 1) and (GPIO.input(6) == 0)): 
+      positie = 448
+      print("<p> De wind komt uit het Zuidoosten </p>")     
+    if ((GPIO.input(26) == 1) and (GPIO.input(19) == 0) and (GPIO.input(13) == 0) and (GPIO.input(6) == 1)): 
+      positie = 192
+      print("<p> De wind komt uit het Noordwesten </p>")     
+    if ((GPIO.input(26) == 0) and (GPIO.input(19) == 0) and (GPIO.input(13) == 1) and (GPIO.input(6) == 1)): 
+      positie = 64
+      print("<p> De wind komt uit het Zuidwesten </p>")         
+  else:  
+    print("<p> Wind te zwak om richting te bepalen </p>") 

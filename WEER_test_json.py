a869c7c56a9a3e7faf251494032c7c452ecc73d8
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-14 22:19:05 +0200

    Update WEER_test_json.py

--- WEER_test_json.py
+++ WEER_test_json.py
@@ -20,3 +20,12 @@ data['people'].append({
 
 with open('data.txt', 'w') as outfile:
     json.dump(data, outfile)
+
+    
+with open('data.txt') as json_file:
+    data = json.load(json_file)
+    for p in data['people']:
+        print('Name: ' + p['name'])
+        print('Website: ' + p['website'])
+        print('From: ' + p['from'])
+        print('')

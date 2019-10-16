1f2bc0903ce8c708e58612d80720c4f137126b67
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-16 11:11:22 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import time
-from w1thermsensor import W1ThermSensor
-sensor = W1ThermSensor()
+
 
 while True:
-    temperature = sensor.get_temperature()
-    print("The temperature is %s celsius" % temperature)
+
+    print( time.time())
     time.sleep(1)

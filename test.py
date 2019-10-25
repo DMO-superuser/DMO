1e5588d697e4e1a77c3986f29c8be4eeb3391136
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-25 15:24:41 +0200

    Update test.py

--- test.py
+++ test.py
@@ -8,4 +8,6 @@ for sensor in W1ThermSensor.get_available_sensors():
     
     print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
 
+binnen_temp = 00000809ac91.id, 00000809ac91.get_temperature())
+print (binnen_temp)
 

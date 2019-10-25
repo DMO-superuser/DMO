25dfbaaf71345fedb05aaf58a98fca6cef597d24
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-25 15:26:17 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ from w1thermsensor import W1ThermSensor
 for sensor in W1ThermSensor.get_available_sensors():
     
     print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+    print (sensor)
 
-binnen_temp = 00000809ac91.id, 00000809ac91.get_temperature())
-print (binnen_temp)
 

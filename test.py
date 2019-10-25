c729608794e9432393c0cd9c1b21669b6aebba60
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-25 15:31:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,6 @@ for sensor in W1ThermSensor.get_available_sensors():
     print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
     print (sensor)
     print (sensor.id)
-    print (sensor.id, sensor.get_temperature())
+    print (sensor.get_temperature())
 
 

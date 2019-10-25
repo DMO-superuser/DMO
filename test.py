b3951f10694c84b7847c4ac0b6f2c9afc205d1c8
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-25 15:23:16 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import Adafruit_DHT
 from w1thermsensor import W1ThermSensor
 
 for sensor in W1ThermSensor.get_available_sensors():
+    
     print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
 
-print("Sensor1 %s has temperature %.2f" % (1, 1.get_temperature()))
-print("Sensor2 %s has temperature %.2f" % (2, 2.get_temperature()))
+

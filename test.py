e1d89a92349c1a820e65b68efbb818b451f7c183
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2019-10-25 15:21:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,5 +7,5 @@ from w1thermsensor import W1ThermSensor
 for sensor in W1ThermSensor.get_available_sensors():
     print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
 
-print("Sensor1 has temperature %.2f" % (1, sensor.get_temperature()))
-print("Sensor2 has temperature %.2f" % (2, sensor.get_temperature()))
+print("Sensor1 %s has temperature %.2f" % (1, 1.get_temperature()))
+print("Sensor2 %s has temperature %.2f" % (2, 2.get_temperature()))

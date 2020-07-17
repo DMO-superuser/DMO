1343ffd5875a7935af8e9eed4c3ac24f26df5e6c
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-07-17 08:37:38 +0200

    Update DMO_buiten.py

--- DMO_buiten.py
+++ DMO_buiten.py
@@ -66,6 +66,11 @@ while True:
     aantal_graden_planeet = 360 - int(positiestring[beginpos_string:eindpos_string])
     aantal_stappen_te_lopen =  int(aantal_graden_planeet * stappen_per_graad)
 
+    # VOOR TESTDOELEINDEN om sneller te testen
+    aantal_stappen_te_lopen = 100
+   
+   
+   
     # NU NAAR POSITIE RIJDEN 
     teller = 1
     while (teller < aantal_stappen_te_lopen):

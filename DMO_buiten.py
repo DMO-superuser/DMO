30e8166d76c0560246f86c60e8f3590bc99dd920
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-07-18 13:53:18 +0200

    Update DMO_buiten.py

--- DMO_buiten.py
+++ DMO_buiten.py
@@ -40,14 +40,14 @@ positiestring     = ""
 positiestring_oud = "leeg"
 
 while True:
-  r = requests.get(url)
+  r = requests.get(url, timeout=4)
   positiestring = r.text
   #print ("Aarde " + positiestring[6:9])
   #print ("Mars " +  positiestring[9:12])
   #print ("Jupiter " + positiestring[12:15])
   #print ("Saturnus " + positiestring[15:18])
-  #print ("positiestring     " + positiestring)
-  #print ("positiestring_oud " + positiestring_oud)
+  print ("positiestring     " + positiestring)
+  print ("positiestring_oud " + positiestring_oud)
 
  # als er een nieuwe positie is ingegeven op de website
   if (positiestring != positiestring_oud):   

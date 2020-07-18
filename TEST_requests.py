6e14883c4780502d5fdecd9c6bcf216128fee0eb
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-07-18 13:51:18 +0200

    Update TEST_requests.py

--- TEST_requests.py
+++ TEST_requests.py
@@ -1,5 +1,5 @@
 import requests
-response = requests.get('http://planetarium.chrisdemoor.nl/positions.txt')
+response = requests.get('http://planetarium.chrisdemoor.nl/positions.txt', timeout = 4)
 print (response.status_code)
 print (response.content)
 print (response.headers)

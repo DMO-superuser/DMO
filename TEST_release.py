854461ebe7c97cc9b53f72780ea0334b5550e413
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-07-22 18:54:40 +0200

    Update TEST_release.py

--- TEST_release.py
+++ TEST_release.py
@@ -1,11 +1,10 @@
 from adafruit_motorkit import MotorKit
-#from adafruit_motor import stepper
+from adafruit_motor import stepper
 kit = MotorKit()
 
 teller = 1
-while (teller < 50):
+while (teller < 50) :
   kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
   teller +=1
 
 kit.stepper1.release()
-   

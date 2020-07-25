d45d357edbf0ac27cbd8ea73cd548e7a6ffd1b98
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-07-25 11:36:16 +0200

    Update TEST_buitensteppers_duurtest.py

--- TEST_buitensteppers_duurtest.py
+++ TEST_buitensteppers_duurtest.py
@@ -1,5 +1,17 @@
 from time import sleep
 
-
+from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
+kit = MotorKit()
 
 while True:
+  
+  teller = 1
+  while (teller < 200) :
+    kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+    teller +=1
+
+  kit.stepper1.release()
+
+  sleep(10)
+

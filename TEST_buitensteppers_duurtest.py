f69684d4c8cbdaae23c1f4d6455ef658f776c825
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-10-14 15:53:27 +0200

    Update TEST_buitensteppers_duurtest.py

--- TEST_buitensteppers_duurtest.py
+++ TEST_buitensteppers_duurtest.py
@@ -4,11 +4,19 @@ from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 kit = MotorKit()
 
+import socket
+planeet = socket.gethostname()
+
+
 while True:
   
   teller = 1
   while (teller < 200) :
-    kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+    if planeet != "DMO-Mars":
+       kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+    else:
+       kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)   
+    
     teller +=1
 
   kit.stepper1.release()

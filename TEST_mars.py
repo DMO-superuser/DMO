3fb2212dc4eaa2a2301cc0f186551af2858be991
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-10-14 14:26:50 +0200

    Update TEST_mars.py

--- TEST_mars.py
+++ TEST_mars.py
@@ -1,3 +1,5 @@
+richting = FORWARD
+
 """Simple test for using adafruit_motorkit with a stepper motor"""
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
@@ -6,4 +8,4 @@ from time import sleep
 kit = MotorKit()
 
 while True:
-  kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+  kit.stepper1.onestep(direction=stepper.richting, style=stepper.DOUBLE)

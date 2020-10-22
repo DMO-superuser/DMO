d96e8c4a5213f6b07ba4d716c851ab3be4c05c7f
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-10-22 13:45:03 +0200

    Update TEST_nema17_rondje.py

--- TEST_nema17_rondje.py
+++ TEST_nema17_rondje.py
@@ -11,7 +11,7 @@ kit = MotorKit()
 
 while True:
  if planeet != "DMO-Mars":
-    kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+    kit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.INTERLEAVE)
  else:
-    kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)   
+    kit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.INTERLEAVE)   
  sleep (0.01)

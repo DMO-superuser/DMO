a95cd2174e94b7cf0c8697533fb0844ea29dc097
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-11-14 13:09:53 +0100

    Update DMO.py

--- DMO.py
+++ DMO.py
@@ -1,5 +1,5 @@
 import logging
-logging.basicConfig(filename='/home/pi/DMO/example.log', level=logging.DEBUG)
+logging.basicConfig(filename='/home/pi/DMO/example.log', filemode='w', level=logging.DEBUG)
 logging.debug('This message should go to the log file')
 logging.info('So should this')
 logging.warning('And this, too')

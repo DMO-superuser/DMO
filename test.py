1f8b407b7eeed8c9e4b60653a2603bcae91aeda1
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-12-01 11:26:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ planeet = socket.gethostname()
 
 richting = 'direction=stepper.BACKWARD'
 stijl   = 'style=stepper.DOUBLE'
+allebei = 'direction=stepper.BACKWARD, style=stepper.DOUBLE'
 
 """Simple test for using adafruit_motorkit with a stepper motor"""
 from adafruit_motorkit import MotorKit
@@ -15,7 +16,7 @@ kit = MotorKit()
 
 while True:
  if planeet != "DMO-Mars":
-    kit.stepper1.onestep(richting, stijl) 
+    kit.stepper1.onestep(allebei) 
  else:
-    kit.stepper1.onestep(richting, stijl) 
+    kit.stepper1.onestep(allebei) 
  #sleep (0.01)

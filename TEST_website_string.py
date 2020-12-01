34e8d58a671040853c8b580de57a2d995838cd28
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-12-01 09:56:10 +0100

    Create TEST_website_string.py

---
+++ TEST_website_string.py
@@ -0,0 +1,14 @@
+# string van website genereren zodat alle mogelijke posities van een planeet getest kunnen worden
+# positie loopt van 001 tot en met 360
+
+# IP adres van de webserver is: hostname -I
+# Apachewebserver heeft als file /var/www/html/positions.txt
+# en is dan te benaderen via http://192.168.178.52/positions.txt
+
+f = open("/var/www/html/positions.txt", "w")
+
+
+for x in range(6):
+ f.write(x)
+
+f.close()

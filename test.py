e1c441421f9e3083308f534bb2c4421fc370bc46
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-12-02 19:28:38 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,4 @@
-import socket
-planeet = socket.gethostname()
-
-
-
-
-allebei = 'direction=stepper.BACKWARD, style=stepper.DOUBLE'
-rich = 'BACKWARD'
-stij = 'DOUBLE'
-
-"""Simple test for using adafruit_motorkit with a stepper motor"""
-from adafruit_motorkit import MotorKit
-from adafruit_motor import stepper
-from time import sleep
-
-kit = MotorKit()
-richting = stepper.BACKWARD
-stijl   = stepper.MICROSTEP
-
-while True:
- kit.stepper1.onestep(direction=richting, style= stijl)
-
- #sleep (0.01)
+dt_string = "12:00"
+bestand = open("/home/pi/DMO/DMO.wifi","a")
+bestand.write (dt_string + " WiFi verbroken \n")
+bestand.close()

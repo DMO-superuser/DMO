4e1cf8b2b6c1d64a6c93a3fecad114c3a76269b3
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-12-09 12:11:05 +0100

    Update TEST_scraper.py

--- TEST_scraper.py
+++ TEST_scraper.py
@@ -1,6 +1,16 @@
-from twill.commands import go, showforms, formclear, fv, submit
+import mechanize
 
-go('https://www.fetlife.com')
-go('./widgets')
-showforms()
+username = ""
+password = ""
+login_post_url = "https://fetlife.com/users/sign_in"
+internal_url = "https://naturalgasintel.com/ext/resources/Data-Feed/Daily-GPI/2018/12/20181221td.txt"
 
+browser = mechanize.Browser()
+browser.open(login_post_url)
+browser.select_form(nr = 1)
+browser.form['Kriz'] = username
+browser.form['Prodeo123'] = password
+browser.submit()
+
+response = browser.open(internal_url)
+print response.read()

914400441e4c587e57e627ecc8f5a0270dc5fe99
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-12-09 12:01:01 +0100

    Update TEST_scraper.py

--- TEST_scraper.py
+++ TEST_scraper.py
@@ -1,12 +1,12 @@
 from twill.commands import go, showforms, formclear, fv, submit
 
-go('http://localhost:8080/')
+go('https://www.fetlife.com')
 go('./widgets')
 showforms()
 
 formclear('1')
-fv('1', 'name', 'test')
-fv('1', 'password', 'testpass')
+fv('1', 'name', 'Kriz')
+fv('1', 'password', 'Prodeo123')
 fv('1', 'confirm', 'yes')
 showforms()
 

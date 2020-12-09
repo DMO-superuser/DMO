c7a9dea7ec6cd12acdf9541e187dd28f8f597259
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-12-09 12:01:58 +0100

    Update TEST_scraper.py

--- TEST_scraper.py
+++ TEST_scraper.py
@@ -4,10 +4,3 @@ go('https://www.fetlife.com')
 go('./widgets')
 showforms()
 
-formclear('1')
-fv('1', 'name', 'Kriz')
-fv('1', 'password', 'Prodeo123')
-fv('1', 'confirm', 'yes')
-showforms()
-
-submit('0')

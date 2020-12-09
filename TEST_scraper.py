4068a9c68f60608ccef27e2c6637cf492f4f3051
DMO-superuser <44121853+DMO-superuser@users.noreply.github.com>
2020-12-09 12:23:34 +0100

    Update TEST_scraper.py

--- TEST_scraper.py
+++ TEST_scraper.py
@@ -6,7 +6,7 @@ login_post_url = "https://fetlife.com/users/sign_in"
 internal_url = "https://naturalgasintel.com/ext/resources/Data-Feed/Daily-GPI/2018/12/20181221td.txt"
 
 browser = mechanize.Browser()
-browser.open(YOUR URL)
+browser.open(login_post_url)
 browser.select_form(nr = 0)
 browser.form['username'] = USERNAME
 browser.form['password'] = PASSWORD
